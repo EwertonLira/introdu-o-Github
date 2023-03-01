02a5e9f8a9496325567ec79a60927cd0aa0d428f
EwertonLira <hectorlira3@gmail.com>
2023-03-01 14:41:08 -0300

    Update arquivo_teste.py

--- pasta exercícios/arquivo_teste.py
+++ pasta exercícios/arquivo_teste.py
@@ -1,15 +1,18 @@
-lista = ['banana','acerola','abacaxi', 'limão', 'uva']
+lista = ['banana','acerola','ACerola','ABacaxi', 'limão', 'uva'] # prioridade é as palavras em maiúscualas seguindo pela ordem alfabética.
+
+print(min(lista))
 
 def numerolista():
-    for i, valor in enumerate(lista,start=1):
-        ix, iy = i + 1 , i + -1
-        print(valor[ix] + valor[iy])
+    for i, valor in enumerate(lista,start=0):
+        ix, iy = i -1 , i 
+        print(lista[ix] +' '+ lista[iy])
 
 numerolista()
 
 lista.append('kiwi')
 print("")
 numerolista()
+
 
 
 
