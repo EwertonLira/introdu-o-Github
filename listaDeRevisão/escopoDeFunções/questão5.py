adb09f873f57f8bd9866fea8dfa1fa5bfc60e90f
EwertonLira <125504635+EwertonLira@users.noreply.github.com>
2023-03-25 23:48:57 -0300

    update

--- listaDeRevisão/escopoDeFunções/questão5.py
+++ listaDeRevisão/escopoDeFunções/questão5.py
@@ -3,6 +3,29 @@
 # e uma estrutura de repetição para percorrer a lista. Em seguida, utilize uma variável global para contar
 # quantas vezes a função foi chamada e imprimir o valor da contagem ao final de cada chamada.
 palavras = ["banana","abacate","acerola","goiaba","kiwi","caju","uva","ameixa","maça","pêra"]
+palavras1 = ["calça","bermuda", "short", "saia", "camiseta"]
+palavras2 = ["Salvador Dalí","michelangelo","leonardo da vinci","donatello","rafael sanzio","Pierre-Auguste Renoir"]
+vezesFuncaoUtilizada = 0
 
-def maiorString(string):
+def maiorString(listaString):
     
+    global vezesFuncaoUtilizada
+    vezesFuncaoUtilizada += 1
+    
+    maiorPalavra = ""
+    
+    for palavra in listaString:
+        
+        if len(maiorPalavra) < len(palavra):
+            maiorPalavra = palavra
+
+    msg = f"""
+    A maior palavra da lista é: {maiorPalavra}.
+    A função foi utilizada {vezesFuncaoUtilizada} vezes.
+    """
+    return msg
+
+print(maiorString(palavras))
+print(maiorString(palavras1))
+print(maiorString(palavras2))
+
