83da8e4cd32393d123a418d097d33e47a2fbab9b
EwertonLira <hectorlira3@gmail.com>
2023-04-26 16:41:44 -0300

    Update classProduto.py

---
+++ BancoDeDados/10 aula(mvc)/model/classProduto.py
@@ -0,0 +1,8 @@
+class Produto:
+    def __init__(self, dbname, host, port, user, password) -> None:
+        
+        self._dbname = dbname
+        self._host = host
+        self._port = port
+        self._user = user
+        self._password = password
