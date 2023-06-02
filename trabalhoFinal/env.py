1f18468850b0ca12d9ffc29dbded32273b0de1ff
EwertonLira <125504635+EwertonLira@users.noreply.github.com>
2023-06-02 11:31:29 -0300

    update

---
+++ trabalhoFinal/env.py
@@ -0,0 +1,9 @@
+
+# variárveis do banco de dado Sun
+dbname = "sun"
+host = "localhost"
+port = "5432"
+user = "postgres"
+password = "postgres" # no computador do cateq mudar para "postgre"
+
+########
